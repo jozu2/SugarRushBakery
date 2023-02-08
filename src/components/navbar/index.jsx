6df928b2012426a7b0239635940a8382d6057bbf
jozu2 <it.joshuamelendres@gmail.com>
2023-02-08 21:43:55 +0800

    first commit

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,21 @@
+import "./style.scss";
+
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="start"></div>
+        <div className="center">
+          <div className="logo">Milk-Tea</div>
+        </div>
+        <div className="end">
+          <div className="hamburger-menu-container"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default index;
